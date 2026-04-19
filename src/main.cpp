7b7b70904f1a3b206efaff81d4468e368a33a0c3
barsik0396 <Barsik0396@outlook.com>
2026-04-19 13:08:38 +0300

    Написал основной файл

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    app.setApplicationName("BarsikTerm");
+
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
